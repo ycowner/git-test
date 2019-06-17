7d4d5cbae895cb6f57c9b8376b598cc31fdbc247
yc <1101281607@qq.com>
2019-06-17 23:04:44 +0800

    添加本地工程到git

---
+++ git_01/src/com/itheima/gitcase/TestOne.java
@@ -0,0 +1,12 @@
+package com.itheima.gitcase;
+
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+}
